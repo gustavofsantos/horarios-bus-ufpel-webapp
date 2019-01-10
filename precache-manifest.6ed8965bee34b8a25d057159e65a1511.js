6f58dc27d54212f05a197dfea1dc131ae3fe3b7b
Gustavo Fernandes dos Santos <gfdsantos@inf.ufpel.edu.br>
2019-01-10 15:38:21 -0200

    Updates

---
+++ precache-manifest.6ed8965bee34b8a25d057159e65a1511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab5c62bb1d06bbcfe2d5",
+    "url": "/horarios-bus-ufpel-webapp/static/js/runtime~main.ab5c62bb.js"
+  },
+  {
+    "revision": "58eebf41afd9b96aa34e",
+    "url": "/horarios-bus-ufpel-webapp/static/js/main.58eebf41.chunk.js"
+  },
+  {
+    "revision": "ca3e6428264a3d3eab72",
+    "url": "/horarios-bus-ufpel-webapp/static/js/1.ca3e6428.chunk.js"
+  },
+  {
+    "revision": "58eebf41afd9b96aa34e",
+    "url": "/horarios-bus-ufpel-webapp/static/css/main.5b3b2d0c.chunk.css"
+  },
+  {
+    "revision": "b48c39127f2a4b482b6a44ad610cace2",
+    "url": "/horarios-bus-ufpel-webapp/index.html"
+  }
+];
